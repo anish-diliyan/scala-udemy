657eeb3380c916232a27476888460bac0663dfa1
Anish Kumar <anish.diliyan@gmail.com>
2025-03-26 10:42:55 +0530

    data source config is reading details from liquibase.properties

--- spark-cmp/src/main/scala/config/DataSourceConfig.scala
+++ spark-cmp/src/main/scala/config/DataSourceConfig.scala
@@ -7,8 +7,14 @@ import javax.sql.DataSource
 object DataSourceConfig {
   def createDataSource(): DataSource = {
     val config = new HikariConfig()
-    config.setJdbcUrl(sys.env.getOrElse("DB_URL", "jdbc:postgresql://localhost:5432/restr_transform_out_pp"))
-    config.setUsername(sys.env.getOrElse("DB_USERNAME", "postgres"))
+    // Read database configuration from liquibase.properties file
+    val props = new java.util.Properties()
+    props.load(getClass.getResourceAsStream("/db/liquibase.properties"))
+
+    // Set database connection properties in config
+    config.setDriverClassName(props.getProperty("driver"))
+    config.setJdbcUrl(props.getProperty("url"))
+    config.setUsername(props.getProperty("username"))
     config.setMaximumPoolSize(5)
 
     new HikariDataSource(config)
--- spark-cmp/src/main/scala/config/LiquibaseMigrate.scala
+++ spark-cmp/src/main/scala/config/LiquibaseMigrate.scala
@@ -4,8 +4,11 @@ import javax.sql.DataSource
 
 class LiquibaseMigrate {
   val dataSource: DataSource = DataSourceConfig.createDataSource()
+  // This line is needed to create a LiquibaseConfig instance that will manage database migrations
+  // Even with liquibase.properties file present, we need to explicitly create the config object
+  // and pass the DataSource to it, as the properties file only contains static configuration
+  // but doesn't handle the runtime database connection setup
   val liquibaseConfig = new LiquibaseConfig(dataSource)
-
   try {
     liquibaseConfig.migrate()
   } catch {
